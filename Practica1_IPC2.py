e09093f6b7f1748e2fb6fe9075c773f850caf765
AbidelLux <3031591380108@ingenieria.usac.edu.gt>
2020-12-07 19:55:57 -0600

    funcion leer definitivo

--- Practica1_IPC2.py
+++ Practica1_IPC2.py
@@ -1,3 +1,8 @@
+
+import json
+cont = ""
+calc = ""
+
 def pedirMenuEntrevistas():
 
     print("Elegir una opcion ingresando un numero entero")
@@ -11,35 +16,44 @@ def pedirMenuEntrevistas():
             print("Por favor ingresar un numero entero")
     return num
 
+
 def leerArchivo():
     ruta = input("Ingresar ruta del archivo: ")
-    archivo = open(ruta, 'r')
-    print("cargando archivo ...."+ruta)
-    print(archivo.read())
+    arch = open(ruta, 'r')
+    print("cargando archivo "+ruta)
+    contenido = arch.read()
+    print(contenido)
+    arch.close()
+    return contenido
 
 
-def calculo():
-    print("calculo")
+def calculo(leer):
+    print(leer)
 
 def generarArchivo():
     print("generando ....")
 
-while True:
-    print("1) Lectura de archivo csv ")
-    print("2) Calculo de datos")
-    print("3) Generacion de archivo Json")
-    print("4) salir del programa")
-    opcion = pedirMenuEntrevistas()
-   #print(opcion)
-    if opcion == 1:
-       leerArchivo()
-    elif opcion == 2:
-       print("calculo de datos")
-       calculo()
-    elif opcion == 3:
-       print("archivo JSON")
-       generarArchivo()
-    elif opcion == 4:
-       break
-    else:
-       print("La opcion",opcion,"no existe")
+def menuPrincipal():
+
+    while True:
+        print("1) Lectura de archivo csv ")
+        print("2) Calculo de datos")
+        print("3) Generacion de archivo Json")
+        print("4) salir del programa")
+        opcion = pedirMenuEntrevistas()
+       #print(opcion)
+        if opcion == 1:
+           cont= leerArchivo()
+        elif opcion == 2:
+           print("calculo de datos")
+           calculo(cont)
+        elif opcion == 3:
+           print("archivo JSON")
+           generarArchivo()
+        elif opcion == 4:
+           break
+        else:
+           print("La opcion",opcion,"no existe")
+
+if __name__ == '__main__':
+    menuPrincipal()
